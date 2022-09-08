5fb22eb6c975f0fb85d50be79bfb93a9499026de
habiibullah <ajiboyhabiib@gmail.com>
2022-09-08 18:10:47 +0100

    Assembler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
 
-/**
- * main - print the string in the put functio 
- *
- * Description : using the main function
- * this program prints "Programming is like building a multilingual puzzle
- * Return: Always:0
- */
-int main(void)
-{
-puts("/"Programming is like building a multilingual puzzle");
-return(0);
-}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line
+script that generates the assembly code of a C code and save it in an output file
