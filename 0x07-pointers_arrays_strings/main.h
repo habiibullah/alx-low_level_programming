7d02e17ece5d8817ddcc01469e33eb6f0ee385fb
habiibullah <ajiboyhabiib@gmail.com>
2022-09-26 13:35:42 +0100

    Write a function that locates a substring

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void simple_print_buffer(char *buffer, unsigned int size);
+char *_strstr(char *haystack, char *needle);
 
 #endif
